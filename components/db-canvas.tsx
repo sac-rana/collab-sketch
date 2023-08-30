c81a1c08a4fdb8e161a08b5bd4ae67f68bcf3967
Sachin Rana <sachinrana1827@gmail.com>
2023-08-30 18:59:19 +0530

    added bucket tool functionality

--- components/db-canvas.tsx
+++ components/db-canvas.tsx
@@ -79,12 +79,37 @@ export default function DBCanvas({ selectedColor, selectedTool }: Props) {
               7,
             );
             canvasLayer1Ctx.current!.fill();
+          } else if (selectedTool == 'bucket') {
+            const imgData = canvasLayer1Ctx.current!.getImageData(
+              0,
+              0,
+              640,
+              420,
+            );
+            let x = e.nativeEvent.offsetX,
+              y = e.nativeEvent.offsetY;
+            let index = (y * imgData.width + x) * 4;
+            let targetColor =
+              imgData.data[index + 3] == 0
+                ? 'rgb(255, 255, 255)'
+                : `rgb(${imgData.data[index]}, ${imgData.data[index + 1]}, ${
+                    imgData.data[index + 2]
+                  })`;
+            let fillColor = canvasLayer1Ctx.current!.fillStyle.toString();
+            let r = parseInt(fillColor.slice(1, 3), 16),
+              g = parseInt(fillColor.slice(3, 5), 16),
+              b = parseInt(fillColor.slice(5, 7), 16);
+
+            if (targetColor == `rgb(${r}, ${g}, ${b})`) return;
+            floodFill(x, y, targetColor, { r, g, b }, imgData);
+            canvasLayer1Ctx.current!.putImageData(imgData, 0, 0);
           }
         }}
         onMouseLeave={e => {
           isDrawing.current = false;
         }}
       ></canvas>
+
       <canvas
         ref={canvas => (canvasLayer2Ctx.current = canvas?.getContext('2d'))}
         style={{
@@ -120,7 +145,6 @@ export default function DBCanvas({ selectedColor, selectedTool }: Props) {
         }}
         onMouseUp={e => {
           if (!isDrawing.current) return;
-          canvasLayer2Ctx.current!.closePath();
           isDrawing.current = false;
           canvasLayer1Ctx.current!.drawImage(
             canvasLayer2Ctx.current!.canvas,
@@ -133,3 +157,34 @@ export default function DBCanvas({ selectedColor, selectedTool }: Props) {
     </div>
   );
 }
+
+const floodFill = (
+  x: number,
+  y: number,
+  targetColor: string,
+  fillColor: { r: number; g: number; b: number },
+  imgData: ImageData,
+) => {
+  const stack = [{ x, y }];
+  while (stack.length != 0) {
+    const { x, y } = stack.pop()!;
+    if (x < 0 || x >= imgData.width || y < 0 || y >= imgData.height) continue;
+    let index = (y * imgData.width + x) * 4;
+    let currColor =
+      imgData.data[index + 3] == 0
+        ? 'rgb(255, 255, 255)'
+        : `rgb(${imgData.data[index]}, ${imgData.data[index + 1]}, ${
+            imgData.data[index + 2]
+          })`;
+    if (currColor != targetColor) continue;
+    imgData.data[index] = fillColor.r;
+    imgData.data[index + 1] = fillColor.g;
+    imgData.data[index + 2] = fillColor.b;
+    imgData.data[index + 3] = 255;
+
+    stack.push({ x: x + 1, y });
+    stack.push({ x: x - 1, y });
+    stack.push({ x, y: y + 1 });
+    stack.push({ x, y: y - 1 });
+  }
+};
